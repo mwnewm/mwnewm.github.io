bb7e0aab543dd30df7139f1e1ea222684421402f
Maeve Newman <maevenewman@gmail.com>
2020-11-22 11:26:36 -0800

    add State props

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,11 +1,40 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      title: 'Maeve Newman',
+      headerLinks: [
+        {title: 'Home', path: '/'},
+        {title: 'About', path: '/about'},
+        {title: 'Resume', path: '/resume'}
+      ],
+      home: {
+        title: 'Title',
+        subtitle: 'Subtitle',
+        substitle2: 'subtitle2'
+      },
+      about: {
+        title: 'About Me',
+        subtitle: '',
+        substitle2: ''
+      },
+      resume: {
+        title: 'Resume',
+        subtitle: '',
+        substitle2: ''
+      }
+    }
+  }
+  
+  render() {
+    return (
+      <div>Hello from React</div>
+    );
+  }
 }
 
 export default App;
