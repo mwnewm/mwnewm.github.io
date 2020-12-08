8136928da437ee7fdac855775390d9bd5dff83ee
Maeve Newman <maevenewman@gmail.com>
2020-12-07 23:25:02 -0800

    NavBar fixes

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ import Container from 'react-bootstrap/Container';
 import './App.css';
 import { NavigationBar } from './components/NavigationBar';
 
-
 import {HomePage} from './pages/HomePage';
 import {UnderConstruction} from './pages/UnderConstruction';
-import pdf from './assets/MaeveNewmanResume.pdf';
 import {NoMatch} from './pages/NoMatch';
 
 class App extends React.Component {
@@ -39,10 +37,21 @@ class App extends React.Component {
       <React.Fragment>
         <Container fluid>
           <Router>
-          <NavigationBar />
+            <NavigationBar />
               <Switch>
-                <Route exact path='/resume' render={() => <body onLoad={window.open(pdf, '_self')}/>}></Route>
-                <Route exact path='/' component={HomePage}></Route>
+                 {/*<Route exact path='/linkedin' component={() => { 
+                  window.location.href = 'https://www.linkedin.com/in/maevewnewman/'; 
+                  return null; 
+                }}/>
+                <Route exact path='/twitter' component={() => { 
+                  window.location.href = 'https://twitter.com/MaeveNewmdev/'; 
+                  return null; 
+                }}/>
+                <Route exact path='/github' component={() => { 
+                  window.location.href = 'https://github.com/mwnewm/'; 
+                  return null; 
+                }}/> */}
+                <Route exact path='/' component={HomePage}></Route> 
                 <Route component={NoMatch}></Route>
               </Switch>
           </Router>
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,17 +3,26 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { MDBIcon } from 'mdbreact';
+import pdf from '../assets/MaeveNewmanResume.pdf';
 
 const Styles = styled.div`
   .navbar {
-    background-color: #404443;
+    background-color: var(--independence);
   }
   a, .navbar-brand, .navbar-nav .nav-link {
     color: #d6d7d8;
     &:hover {
       color: white;
     }
+    &:visited {
+      color: #d6d7d8;
+    }
   }
+  .nav-icon {
+    padding-right: 10px;
+  }
+  
 `;
 
 export const NavigationBar = () => (
@@ -21,9 +30,19 @@ export const NavigationBar = () => (
     <Navbar className='border-bottom' expand='lg'>
       <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
       <Navbar.Collapse id="navbar-toggle">
+        <Nav className='p-2'>
+          <Link className='nav-link' to={{ pathname: "https://www.linkedin.com/in/maevewnewman/" }} target="_blank">LinkedIn
+            {/* <MDBIcon fab icon="linkedin" size="2x" className='nav-icon' /> */}
+          </Link>
+          <Link className='nav-link' to={{ pathname: "https://twitter.com/MaeveNewmdev/" }} target="_blank">Twitter
+            {/* <MDBIcon fab icon="twitter-square" size="2x" className='nav-icon' /> */}
+          </Link>
+          <Link className='nav-link' to={{pathname: "https://github.com/mwnewm/"}} target="_blank">Github
+            {/* <MDBIcon fab icon="github-square" size="2x" className='nav-icon' /> */}
+          </Link>
+        </Nav>
         <Nav className="ml-auto">
-          <Link className="nav-link" to="/">Home</Link>
-          <Link className="nav-link" to="/resume">Resume</Link>
+          <a className="nav-link" href={pdf}>Resume</a>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
